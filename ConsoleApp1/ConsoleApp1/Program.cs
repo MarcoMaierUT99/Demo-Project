1d19ef43316888ab188aa0b2d89e6f2ef7354874
MarcoMaierUT99 <35327101+MarcoMaierUT99@users.noreply.github.com>
2018-01-11 10:42:57 +0100

    Texte korrigiert

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hallo welt!");
+            Console.WriteLine("Hallo Welt!");
             Console.ReadLine();
         }
     }
